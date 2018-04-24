a0422d09ec054389328fb16bbf3635209a5f0228
dlipinski <dawid.lipinski3@gmail.com>
2018-04-25 00:58:03 +0200

    Probably working Mastermind : )

---
+++ lab5/zad4.js
@@ -0,0 +1,49 @@
+/* jshint strict: global, esversion: 6, devel: true, node: true */
+'use strict';
+
+
+var  kod= [ 1,2,3,4];
+
+var ruch = [ 1,3,6,2];
+
+const ocena = (kod) => {
+    return (ruch) => {
+
+        // Check if kod and ruch are equal length
+        if (kod.length !== ruch.length){
+            throw ({ typerr: "Arrays are not equal length!" });
+        }
+
+        //Init arrays for black and white indexes
+        let black =[];
+        let white = [];
+
+        // Find blacks and remember position
+        kod.forEach( (value, index) => {
+            if(value===ruch[index]){
+                black.push(index);
+                console.log("Black found: " + value);
+            }
+        });
+
+        // Find whites and remember position
+        ruch.forEach( (value, index) => {
+            if(kod.includes(value) && !black.includes(index) && !white.includes(index)){
+                white.push(index);
+                console.log("White found: " + value);
+            }
+        });
+
+        return {biale: white.length, czarne: black.length};
+    };
+};
+
+
+try{
+    let myOcena = ocena (kod)(ruch);
+    console.log('Białe: '+myOcena.biale);
+    console.log('Czarne: '+myOcena.czarne);
+} catch (e) {
+    console.log(e.typerr);
+}
+
